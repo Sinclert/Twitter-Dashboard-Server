f8a24bf1d20c9c6eb30a141b8e113f330a52029d
Sinclert <sinclert@hotmail.com>
2019-05-26 21:55:21 +0200

    Refactor file.py functions arguments

--- src/utils/files.py
+++ src/utils/files.py
@@ -17,75 +17,75 @@ file_types = {
 
 
 
-def compute_path(file_folder: str, file_type: str, file_name: str) -> str:
+def compute_path(f_folder: str, f_type: str, f_name: str) -> str:
 
 	"""
 	Builds the absolute path to the desired file
 
-	:param file_folder: folder where the file types are
-	:param file_type: type of the file
-	:param file_name: name of the file
+	:param f_folder: folder where the file types are
+	:param f_type: type of the file
+	:param f_name: name of the file
 	"""
 
 	try:
 		project_root = str(os.path.dirname(os.getcwd()))
 
 		path = [project_root]
-		path = path + file_folders[file_folder]
-		path = path + file_types[file_type]
-		path = path + [file_name]
+		path = path + file_folders[f_folder]
+		path = path + file_types[f_type]
+		path = path + [f_name]
 
 		return os.path.join(*path)
 
 	except KeyError:
-		exit('The file type "' + file_type + '" is not defined')
+		exit('The file type "' + f_type + '" is not defined')
 
 
 
 
-def load_object(file_folder: str, file_type: str, file_name: str) -> dict:
+def load_object(f_folder: str, f_type: str, f_name: str) -> dict:
 
 	"""
-	Loads an object from the specified file
+	Loads a serialized object from the specified file
 
-	:param file_folder: folder where the file types are
-	:param file_type: type of the file
-	:param file_name: name of the file
+	:param f_folder: folder where the file types are
+	:param f_type: type of the file
+	:param f_name: name of the file
 	"""
 
-	file_path = compute_path(file_folder, file_type, file_name)
+	path = compute_path(f_folder, f_type, f_name)
 
 	try:
-		file = open(file_path, 'rb')
+		file = open(path, 'rb')
 		obj = pickle.load(file)
 		file.close()
 
 		return obj
 
 	except IOError:
-		exit('The object could not be loaded from ' + file_path)
+		exit('The object could not be loaded from ' + path)
 
 
 
 
-def read_json(file_folder: str, file_type: str, file_name: str) -> dict:
+def read_json(f_folder: str, f_type: str, f_name: str) -> dict:
 
 	"""
 	Reads a JSON file and returns it as a dictionary
 
-	:param file_folder: folder where the file types are
-	:param file_type: type of the file
-	:param file_name: name of the file
+	:param f_folder: folder where the file types are
+	:param f_type: type of the file
+	:param f_name: name of the file
 	"""
 
-	file_path = compute_path(file_folder, file_type, file_name)
+	path = compute_path(f_folder, f_type, f_name)
 
 	try:
-		file = open(file_path, 'r', encoding='utf-8')
+		file = open(path, 'r', encoding='utf-8')
 		json_dict = json.load(file)
 		file.close()
 
 		return json_dict
 
 	except IOError:
-		exit('The file ' + file_name + ' cannot be opened')
+		exit('The file ' + f_name + ' cannot be opened')
